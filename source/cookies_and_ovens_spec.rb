44e4f3b89bff0cd932a0029d4cb050f13d76492f
Matthew DeCarlo <matthewwho@me.com>
2015-01-02 17:45:11 -0600

    rSpec

--- source/cookies_and_ovens_spec.rb
+++ source/cookies_and_ovens_spec.rb
@@ -1,3 +1,21 @@
 require_relative 'cookies_and_ovens'
 
-# Write your tests here.  We'd write some for you, but who knows what classes you'll use!
+describe Bakery do
+  it 'has an empty oven' do
+    a_bakery = Bakery.new
+    expect(a_bakery.oven).to eq([])
+  end
+
+  it 'has an empty recipe holder' do
+    a_bakery = Bakery.new
+    expect(a_bakery.recipes).to eq([])
+  end
+  
+  describe '.get_recipes' do
+    it 'reads csv file, and puts each recipe into the recipe array' do
+      a_bakery = Bakery.new
+      a_bakery.get_recipes('cookies.csv')
+      expect(a_bakery.recipes.length).to be >= 1      
+    end
+  end
+end
